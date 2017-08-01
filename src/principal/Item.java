7611fbed53e82cfaad67c215d666f1e2e4bd0500
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-08-01 19:40:19 -0300

    basico de Item implementado

--- src/principal/Item.java
+++ src/principal/Item.java
@@ -1,5 +1,28 @@
 package principal;
 
-public class Item {
+public abstract class Item {
+	String nome;
+	double valor;
+	boolean emprestado;
+
+	public Item(String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+		this.emprestado = false;
+	}
+	
+	public boolean isEmprestado() {
+		return emprestado;
+	}
+
+	public void setEmprestado() {
+		this.emprestado = true ? !isEmprestado() : false;
+	}
+	
+	public abstract int hashCode();
+	
+	public abstract boolean equals();
+	
+	public abstract String toString();
 
 }
