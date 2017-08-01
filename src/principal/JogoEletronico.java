7b062fc2f925eb0f2d89a18d348f1522e24e29c2
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-08-01 20:57:30 -0300

    implementacao de jogo eletronico

---
+++ src/principal/JogoEletronico.java
@@ -0,0 +1,50 @@
+package principal;
+
+public class JogoEletronico extends Item {
+
+	private String plataforma;
+
+	public JogoEletronico(String nome, double valor, String plataforma) {
+		super(nome, valor);
+		this.plataforma = plataforma;
+	}
+	
+	@Override
+	public String toString() {
+		String situacao = this.emprestado ? "EMPRESTADO" : "COM DONO";
+		return this.nome + " - " + this.plataforma + " - " + this.valor + " - " + 
+				situacao;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((plataforma == null) ? 0 : plataforma.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JogoEletronico other = (JogoEletronico) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (plataforma == null) {
+			if (other.plataforma != null)
+				return false;
+		} else if (!plataforma.equals(other.plataforma))
+			return false;
+		return true;
+	}
+
+}
