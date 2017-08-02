cae574ad020057632c6cdbc962825858ac4a8d86
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2017-08-02 15:09:48 -0300

    Métodos add

--- src/principal/Facade.java
+++ src/principal/Facade.java
@@ -3,28 +3,86 @@ package principal;
 import easyaccept.EasyAccept;
 
 public class Facade {
-	public static void main(String[] args){
-		args = new String[]{"principal.Facade", "acceptance_test/us1_test.txt"};
+	public static void main(String[] args) {
+		args = new String[] { "principal.Facade", "acceptance_test/us1_test.txt" };
 		EasyAccept.main(args);
 	}
+
 	private UserController sistema;
 
-	public void iniciarSistema(){
+	public void iniciarSistema() {
 		sistema = new UserController();
 	}
-	public void cadastrarUsuario(String nome, String celular, String email){
+
+	public void fecharSistema() {
+		sistema.fechaSistema();
+	}
+
+	public void cadastrarUsuario(String nome, String celular, String email) {
 		sistema.cadastraUsuario(nome, celular, email);
 	}
-	public void removerUsuario(String nome, String celular){
+
+	public void removerUsuario(String nome, String celular) {
 		sistema.removeUsuario(nome, celular);
 	}
-	public String getInfoUsuario(String nome, String celular, String atributo){
+
+	public String getInfoUsuario(String nome, String celular, String atributo) {
 		return sistema.getInfoUsuario(nome, celular, atributo);
 	}
-	public void atualizarUsuario(String nome, String celular, String atributo, String valor){
-		sistema.atualizaUsuario(nome, celular, atributo, valor);
+
+	public void atualizarUsuario(String nome, String telefone, String atributo, String valor) {
+		sistema.atualizaUsuario(nome, telefone, atributo, valor);
 	}
-	public void fecharSistema(){
-		sistema.fechaSistema();
+	
+	public void cadastrarEletronico(String nome, String telefone, String nomeItem, double preco, String plataforma) {
+		
+	}
+	
+	public void cadastrarJogoTabuleiro(String nome, String telefone, String nomeItem, double preco) {
+		
+	}
+	
+	public void adicionarPecaPerdida(String nome, String telefone, String nomeItem, String nomePeca) {
+		
+	}
+	
+	public void cadastrarBluRayFilme(String nome, String telefone, String nomeItem, double preco, int duracao, String genero, String classificacao) {
+		
+	}
+	
+	public void cadastrarBluRayShow(String nome, String telefone, String nomeItem, double preco, int duracao, int faixas, String artista, String classificacao) {
+		
+	}
+	
+	public void cadastrarBluRaySerie(String nome, String telefone, String nomeItem, double preco, String descricao, int duracao, String classificacao, String genero, int temporada) {
+		
+	}
+	
+	public void adicionarBluRay(String nome, String telefone, String nomeBluRayTemporada, int duracao) {
+		
+	}
+	
+	public void removerItem(String nome, String telefone, String nomeItem) {
+		
+	}
+	
+	public void atualizarItem(String nome, String telefone, String nomeItem, String atributo, String valor) {
+		
+	}
+	
+	public void listarItensOrdenadosPorNome() {
+		
+	}
+	
+	public void listarItensOrdenadosPorValor() {
+		
+	}
+	
+	public void registrarEmprestimo(String nomeDono, String telefoneDono, String nomeRequerente, String telefoneRequerente, String nomeItem, String dataEmprestimo, int periodo) {
+		
+	}
+	
+	public void devolverItem(String nomeDono, String telefoneDono, String nomeRequerente, String telefoneRequerente, String nomeItem, String dataEmprestimo, String dataDevolucao) {
+		
 	}
 }
