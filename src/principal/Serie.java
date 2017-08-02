733306d3bb602a8a77759dfc6991001d46a7665b
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2017-08-01 21:54:28 -0300

    Classe Serie add

---
+++ src/principal/Serie.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class Serie extends BluRay {
+
+	private String genero;
+	private int temporada;
+
+	public Serie(String nome, double valor, int duracao, String classificacao, String genero, int temporada) {
+		super(nome, valor, duracao, classificacao);
+		this.genero = genero;
+		this.temporada = temporada;
+	}
+
+	public String toString() {
+		return "SERIE: " + this.nome + ", R$ " + this.valor + ", " + this.status() + ", " + this.duracao + " min" + ", "
+				+ this.classificacao + ", " + this.genero + ", Temporada " + this.temporada;
+	}
+}
