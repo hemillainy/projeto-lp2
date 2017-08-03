2bc2ca245c568cae2a47c27bfc2081fb4d6151a4
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2017-08-02 21:53:57 -0300

    refatoraçoes

--- src/principal/UserController.java
+++ src/principal/UserController.java
@@ -4,12 +4,11 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class UserController {
-	private IdUsuario id;
 	private boolean aberto;
 	private Validacao validacao;
 	private Map<IdUsuario, Usuario> usuarios;
 
-	public UserController() { 
+	public UserController() {
 		aberto = true;
 		usuarios = new HashMap<>();
 		validacao = new Validacao();
@@ -27,58 +26,58 @@ public class UserController {
 	}
 
 	public void cadastraUsuario(String nome, String telefone, String email) {
-		id = new IdUsuario(nome, telefone);
 		validacao.dadoUsuarioInvalido(nome, telefone, email);
-		if (!hasUsuario(id)) {
-			Usuario usuario = new Usuario(nome, telefone, email);
-			usuarios.put(id, usuario);
-		} else {
+		IdUsuario id = new IdUsuario(nome, telefone);
+		if (hasUsuario(id)) {
 			validacao.usuarioJaCadastrado();
 		}
+		Usuario usuario = new Usuario(nome, telefone, email);
+		usuarios.put(id, usuario);
 	}
 
 	public void removeUsuario(String nome, String telefone) {
-		id = new IdUsuario(nome, telefone);
-		if (hasUsuario(id)) {
-			usuarios.remove(id);
-		} else {
+		IdUsuario id = new IdUsuario(nome, telefone);
+		if (!hasUsuario(id)) {
 			validacao.usuarioInvalido();
 		}
+		usuarios.remove(id);
 	}
 
 	public String getInfoUsuario(String nome, String telefone, String atributo) {
-		id = new IdUsuario(nome, telefone);
-		if (hasUsuario(id)) {
-			if (atributo.equals("Email")) {
-				return usuarios.get(id).getEmail();
-			} else if (atributo.equals("Nome")) {
-				return usuarios.get(id).getNome();
-			}
+		String info = "";
+		IdUsuario id = new IdUsuario(nome, telefone);
+		if (!hasUsuario(id)) {
+			return validacao.usuarioInvalido();
 		}
-		return validacao.usuarioInvalido();
+		if (atributo.equals("Email")) {
+			info = usuarios.get(id).getEmail();
+		} else if (atributo.equals("Nome")) {
+			info = usuarios.get(id).getNome();
+		}
+		return info;
 	}
 
 	private void setKey(String nome, String telefone, String valor, Usuario usuario) {
-		id = new IdUsuario(nome, telefone);
+		IdUsuario id = new IdUsuario(nome, telefone);
 		usuarios.remove(id);
 		IdUsuario novoId = new IdUsuario(nome, valor);
 		usuarios.put(novoId, usuario);
 	}
 
 	public void atualizaUsuario(String nome, String telefone, String atributo, String valor) {
-		id = new IdUsuario(nome, telefone);
-		if (hasUsuario(id)) {
-			if (atributo.equals("Email")) {
-				usuarios.get(id).setEmail(valor);
-			} else if (atributo.equals("Telefone")) {
-				Usuario usuario = usuarios.get(id);
-				usuario.setTelefone(valor);
-				setKey(nome, telefone, valor, usuario);
-			}
-		} else {
+		IdUsuario id = new IdUsuario(nome, telefone);
+		if (!hasUsuario(id)) {
 			validacao.usuarioInvalido();
 		}
+		if (atributo.equals("Email")) {
+			usuarios.get(id).setEmail(valor);
+		} else if (atributo.equals("Telefone")) {
+			Usuario usuario = usuarios.get(id);
+			usuario.setTelefone(valor);
+			setKey(nome, telefone, valor, usuario);
+		}
 	}
-	public String pesquisaDetalhesItem(String nome, String telefone, String item){
+
+	public String pesquisaDetalhesItem(String nome, String telefone, String item) {
 	}
 }
