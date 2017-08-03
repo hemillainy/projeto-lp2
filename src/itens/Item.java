69ea0a03a707604b05f5295f90d5f92a5caadad5
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2017-08-02 23:52:07 -0300

    alterado o atributo 'valor' para 'preco'

--- src/itens/Item.java
+++ src/itens/Item.java
@@ -2,20 +2,20 @@ package itens;
 
 public abstract class Item implements Comparable<Item> {
 	protected String nome;
-	protected double valor;
+	protected double preco;
 	protected Status status;
 
 	public Item(String nome, double valor) {
 		this.nome = nome;
-		this.valor = valor;
+		this.preco = valor;
 		this.status = Status.NAO_EMPRESTADO;
 	}
 
 	public void setStaus() {
 		status = verificaEmprestado() ? Status.NAO_EMPRESTADO : Status.EMPRESTADO;
 	}
-	public double getValor(){
-		return this.valor;
+	public String getValor(){
+		return String.format("%.f", preco);
 	}
 
 	private boolean verificaEmprestado() {
