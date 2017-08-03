7c1c87d8dd7d55b022e9fc36b6857fdced71a032
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-08-03 16:09:49 -0300

    resolve conflito

--- src/enums/StatusCompleto.java
+++ src/enums/StatusCompleto.java
@@ -2,12 +2,8 @@ package enums;
 
 public enum StatusCompleto {
 	COMPLETO("SEM PECAS PERDIDAS"),
-<<<<<<< HEAD:src/enums/StatusCompleto.java
 	INCOMPLETO("COM PECAS PERDIDAS");
-=======
-	INCOMPLETO(" PECAS PERDIDAS");
 	
->>>>>>> 685b12e2f81ce6c47005256ead7197d0399a7660:src/itens/jogos/Status.java
 	private final String valor;
 	
 	private StatusCompleto(String valor) {
