1092d00a3ea2c6196575de295e58513ffb862e6c
CasEduardo <cassio.cordeiro@ccc.ufcg.edu.br>
2017-08-10 17:34:42 -0300

    Mais documentacao

--- Tests/principal/IdUsuarioTest.java
+++ Tests/principal/IdUsuarioTest.java
@@ -5,19 +5,34 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**.
+ * Teste da classe IdUsuario.
+ * 
+ * Projeto de Laboratorio de Progamacao 2 - 2017.1 (TT - Tracking things)
+ * 
+ * @author Cassio Cordeiro - 116210038
+ * 		   Geovane Silva - 116211149
+ * 		   Hemillainy Santos - 116210802
+ *
+ */
 public class IdUsuarioTest {
 	
 	IdUsuario id1;
 	IdUsuario id2;
+	IdUsuario id3;
 	
+	/**
+	 * Cria os IdUsuarios necessario para os testes.
+	 */
 	@Before
 	public void criaIdUsuario() {
 		 id1 = new IdUsuario("Cássio", "1234");
 		 id2 = new IdUsuario("Geovane", "123");
+		 id3 = new IdUsuario("Cássio", "1234");
 	}
 	
 	/**
-	 * Teste do metodo toString
+	 * Teste do metodo toString.
 	 */
 	@Test
 	public void testToString() {
@@ -25,12 +40,11 @@ public class IdUsuarioTest {
 	}
 	
 	/**
-	 * Teste do metodo equals
+	 * Teste do metodo equals.
 	 */
 	@Test
 	public void testEqualsObject() {
 		assertFalse(id1.equals(id2));
-		IdUsuario id3 = new IdUsuario("Cássio", "1234");
 		assertTrue(id1.equals(id3));
 	}
 
