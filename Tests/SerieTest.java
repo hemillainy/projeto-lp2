5ab5972b0adfda6d6cbee85d7acbd3ea0736aaca
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2017-08-10 17:36:53 -0300

    documentado

--- Tests/SerieTest.java
+++ Tests/SerieTest.java
@@ -5,15 +5,30 @@ import org.junit.Test;
 
 import itens.blurays.Serie;
 
+/**
+ * Testes da classe Serie.
+ * 
+ * Projeto de Laboratorio de Progamacao 2 - 2017.1 (TT - Tracking things)
+ * 
+ * @author Cassio Cordeiro - 116210038 Geovane Silva - 116211149 Hemillainy
+ *         Santos - 116210802
+ *
+ */
 public class SerieTest {
 
 	Serie serie;
 
+	/**
+	 * Instancia uma serie para ser utilizada no teste.
+	 */
 	@Before
 	public void criaSerie() {
 		serie = new Serie("Arrow", 35.63, 43, "DOZE_ANOS", "Acao", 5);
 	}
 
+	/**
+	 * Testa o toString de Serie.
+	 */
 	@Test
 	public void testToString() {
 		assertEquals("SERIE: Arrow, R$ 35.63, Nao emprestado, 43 min, DOZE_ANOS, Acao, Temporada 5", serie.toString());
