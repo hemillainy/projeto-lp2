eed509c3165d796fb31c6b4f99fc21d66ce657f8
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2017-08-11 17:59:41 -0300

    Script

--- gith.sh
+++ gith.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 read -p "Comentário: " comentario
+read -p "Branch: " branch
 
 function enviar() {
 	git add .
 	git commit -m $comentario
-	git push origin dudu
+	git push origin $branch
 }
+
 enviar
