1a7508c3f5aaeb23d5b4aa68c893c5a8683c4a9c
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2017-08-14 21:53:05 -0300

    Começo da us6

--- src/principal/Facade.java
+++ src/principal/Facade.java
@@ -16,7 +16,8 @@ import easyaccept.EasyAccept;
 public class Facade {
 	public static void main(String[] args) {
 		args = new String[] { "principal.Facade", "acceptance_test/us1_test.txt", "acceptance_test/us2_test.txt",
-				"acceptance_test/us3_test.txt", "acceptance_test/us4_test.txt", "acceptance_test/us5_test.txt" };
+				"acceptance_test/us3_test.txt", "acceptance_test/us4_test.txt", "acceptance_test/us5_test.txt",
+				"acceptance_test/us6_test.txt"};
 		EasyAccept.main(args);
 	}
 
