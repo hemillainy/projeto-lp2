a093df6ecf2ce5768264e7086a0604ed83d98d40
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2017-08-16 13:45:53 -0300

    add listarItensNaoEmprestados

--- src/principal/Listador.java
+++ src/principal/Listador.java
@@ -1,8 +1,13 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import principal.emprestimo.Emprestimo;
+import principal.item.Item;
 import principal.user.Usuario;
 
 public class Listador {
@@ -28,6 +33,28 @@ public class Listador {
 			return "Nenhum item emprestado";
 		}
 		return retorno;
+	}
+	
+	/**
+	 * Metodo que lista os itens cadastrados nao emprestados.
+	 * 
+	 * @return a listagem dos itens nao cadastrados.
+	 */
+	public String listaItensNaoEmprestados(List<Usuario> usuarios) {
+		Set<Item> itens = new HashSet<>();
+		for (Usuario us : usuarios) {
+			itens.addAll(us.getItens());
+		}
+		List<Item> inventario = new ArrayList<>(itens);
+		Collections.sort(inventario, new ComparadorNomeItem());
+
+		String saida = "";
+		for (Item item : itens) {
+			if (item.getStatus().equals("Nao emprestado")) {
+				saida += item.toString() + "|";
+			}
+		}
+		return saida;
 	}
 
 }
