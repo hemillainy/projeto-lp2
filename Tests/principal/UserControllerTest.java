dbd8b1dab578a1c41943d53d03575ddae593ed1e
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-08-22 16:11:38 -0300

    mais testes de userController

--- Tests/principal/UserControllerTest.java
+++ Tests/principal/UserControllerTest.java
@@ -1,9 +1,6 @@
 package principal;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,7 +23,7 @@ import principal.user.Usuario;
 public class UserControllerTest {
 
 	UserController uc = new UserController();
-	
+
 	@Before
 	public void inicializa() {
 		uc.cadastraUsuario("Geovane", "1234-5678", "geovane@email.com");
@@ -39,37 +36,37 @@ public class UserControllerTest {
 		Usuario us = new Usuario("Geovane", "1234-5678", "geovane@email.com");
 		assertEquals(us, uc.getUsuario("Geovane", "1234-5678"));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioNaoCadastrado() {
 		uc.getUsuario("Geo", "1234-5678");
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioNomeNulo() {
 		uc.getUsuario(null, "1234-5678");
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioNomeVazio() {
 		uc.getUsuario("", "1234-5678");
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioNomeEspacos() {
 		uc.getUsuario("   ", "1234-5678");
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioTelefoneNulo() {
 		uc.getUsuario("Geovane", null);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioTelefoneVazio() {
 		uc.getUsuario("Geovane", "");
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetUsuarioTelefoneEspacos() {
 		uc.getUsuario("Geovane", "   ");
@@ -80,92 +77,69 @@ public class UserControllerTest {
 		Usuario us = new Usuario("Geovane", "1234-5678", "geovane@email.com");
 		assertEquals(us, uc.pegarEmprestado("Geovane", "1234-5678", 5));
 	}
-	
+
 	@Test
 	public void testPegarEmprestadoUsuarioinexistente() {
 		Usuario us = new Usuario("Geovane", "1234-5678", "geovane@email.com");
 		assertEquals(us, uc.pegarEmprestado("Geovane", "1234-5678", 5));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoNaoPodePegarEmprestado() {
 		uc.getUsuario("Geovane", "1234-5678").addReputacao(-200, 0.01);
 		uc.pegarEmprestado("Geovane", "1234-5678", 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoNomeNulo() {
 		uc.pegarEmprestado(null, "1234-5678", 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoNomeVazio() {
 		uc.pegarEmprestado("", "1234-5678", 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoNomeEspacos() {
 		uc.pegarEmprestado("   ", "1234-5678", 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoTelefoneNulo() {
 		uc.pegarEmprestado("Geovane", null, 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoTelefoneVazio() {
 		uc.pegarEmprestado("Geovane", "", 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarEmprestadoTelefoneEspacos() {
 		uc.pegarEmprestado("Geovane", "   ", 5);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 	@Test
 	public void testGetUsuarios() {
-		assertEquals("", uc);
+		assertEquals(3, uc.getUsuarios().size());
 	}
 
 	@Test
 	public void testCadastraUsuario() {
-		assertEquals("", uc);
+		uc.cadastraUsuario("GEEEO", "123", "123@email.com");
+		assertEquals(new Usuario("GEEEO", "123", "123@email.com"), uc.getUsuario("GEEEO", "123"));
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testRemoveUsuario() {
-		Usuario us1 = new Usuario("Cassio", "4321-5678", "cassio@email.com");
-		Usuario us2 = new Usuario("Hemillainy", "5678-5678", "hemillainy@email.com");
-		List<Usuario> restantes = new ArrayList<>();
-		restantes.add(us1);
-		restantes.add(us2);
 		uc.removeUsuario("Geovane", "1234-5678");
-		assertEquals(restantes, uc.getUsuarios());
+		uc.getUsuario("Geovane", "1234-5678");
 	}
 
 	@Test
 	public void testGetInfoUsuario() {
-		assertEquals("", uc);
+		assertEquals("geovane@email.com", uc.getInfoUsuario("Geovane", "1234-5678", "email"));
 	}
 
 	@Test
@@ -174,7 +148,7 @@ public class UserControllerTest {
 		uc.atualizaUsuario("Geovane", "1234-5678", "email", "geo@email.com");
 		assertEquals(us, uc.getUsuario("Geovane", "1234-5678"));
 	}
-	
+
 	@Test
 	public void testAtualizaUsuarioTelfone() {
 		Usuario us = new Usuario("Geovane", "2468-13579", "geovane@email.com");
@@ -183,8 +157,15 @@ public class UserControllerTest {
 	}
 
 	@Test
-	public void testListarCaloteiros() {
-		assertEquals("", uc);
+	public void testListarCaloteirosComCaloteiro() {
+		uc.getUsuario("Hemillainy", "5678-5678").addReputacao(-1000, 0.01);
+		assertEquals("Lista de usuarios com reputacao negativa: Hemillainy, hemillainy@email.com, 5678-5678|",
+				uc.listarCaloteiros());
+	}
+
+	@Test
+	public void testListarCaloteirosSemCaloteiro() {
+		assertEquals("Lista de usuarios com reputacao negativa: ", uc.listarCaloteiros());
 	}
 
 	@Test
@@ -192,12 +173,17 @@ public class UserControllerTest {
 		uc.getUsuario("Geovane", "1234-5678").cadastraItem(new JogoEletronico("Naruto", 200, "PC"));
 		uc.getUsuario("Cassio", "4321-5678").cadastraItem(new JogoTabuleiro("Chess", 10000));
 		uc.getUsuario("Hemillainy", "5678-5678").cadastraItem(new Serie("GOT", 180, 220, "DEZOITO_ANOS", "DRAMA", 1));
-		assertEquals("1: Cassio - Reputacao: 500,00|2: Geovane - Reputacao: 10,00|3: Hemillainy - Reputacao: 9,00|", uc.listarTop10MelhoresUsuario());
+		assertEquals("1: Cassio - Reputacao: 500,00|2: Geovane - Reputacao: 10,00|3: Hemillainy - Reputacao: 9,00|",
+				uc.listarTop10MelhoresUsuario());
 	}
 
 	@Test
 	public void testListarTop10PioresUsuario() {
-		assertEquals("", uc);
+		uc.getUsuario("Geovane", "1234-5678").cadastraItem(new JogoEletronico("Naruto", 200, "PC"));
+		uc.getUsuario("Cassio", "4321-5678").cadastraItem(new JogoTabuleiro("Chess", 10000));
+		uc.getUsuario("Hemillainy", "5678-5678").cadastraItem(new Serie("GOT", 180, 220, "DEZOITO_ANOS", "DRAMA", 1));
+		assertEquals("1: Hemillainy - Reputacao: 9,00|2: Geovane - Reputacao: 10,00|3: Cassio - Reputacao: 500,00|",
+				uc.listarTop10PioresUsuario());
 	}
 
 }
